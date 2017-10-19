91d9bd9caba33c7d613784cd4022956ee05527ae
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2017-10-19 12:12:38 -0200

    Auxiliar Module Monitor

--- power_jeston.sh
+++ power_jeston.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-#IPMI_TEMP_OUTPUT=`/usr/sbin/ipmi-sensors`
-### Power and Temperature ###
-#CURRENT_TEMP1=$(echo $IPMI_TEMP_OUTPUT | /usr/sbin/ipmi-sensors | grep CPU1 | grep Temp | awk '{print $8}')
-#CURRENT_TEMP2=$(echo $IPMI_TEMP_OUTPUT | /usr/sbin/ipmi-sensors | grep CPU2 | grep Temp | awk '{print $8}')
+file=/home/nvidia/Testes_ARM/Monitor/power.dat
 
-#echo "$CURRENT_TEMP1  $CURRENT_TEMP2" >> tempetature.dat
-saida_file=/home/nvidia/Testes_ARM/Monitor/power.dat
-
-date +%H:%M:%S.%N >> $saida_file
-echo -n "Total:" >> $saida_file
-cat /sys/bus/i2c/drivers/ina3221x/0-0041/iio_device/in_power0_input >> $saida_file
-echo -n "GPU:" >> $saida_file
-cat /sys/bus/i2c/drivers/ina3221x/0-0040/iio_device/in_power0_input >> $saida_file
-echo -n "CPU:" >> $saida_file
-cat /sys/bus/i2c/drivers/ina3221x/0-0041/iio_device/in_power1_input >> $saida_file
-echo -n "Memory:" >> $saida_file
-cat /sys/bus/i2c/drivers/ina3221x/0-0041/iio_device/in_power2_input >> $saida_file
+date +%H:%M:%S.%N >> $file
+echo -n "Total:" >> $file
+cat /sys/bus/i2c/drivers/ina3221x/0-0041/iio_device/in_power0_input >> $file
+echo -n "GPU:" >> $file
+cat /sys/bus/i2c/drivers/ina3221x/0-0040/iio_device/in_power0_input >> $file
+echo -n "CPU:" >> $file
+cat /sys/bus/i2c/drivers/ina3221x/0-0041/iio_device/in_power1_input >> $file
+echo -n "Memory:" >> $file
+cat /sys/bus/i2c/drivers/ina3221x/0-0041/iio_device/in_power2_input >> $file
 
--- temp_jetson.sh
+++ temp_jetson.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-#IPMI_TEMP_OUTPUT=`/usr/sbin/ipmi-sensors`
-### Power and Temperature ###
-#CURRENT_TEMP1=$(echo $IPMI_TEMP_OUTPUT | /usr/sbin/ipmi-sensors | grep CPU1 | grep Temp | awk '{print $8}')
-#CURRENT_TEMP2=$(echo $IPMI_TEMP_OUTPUT | /usr/sbin/ipmi-sensors | grep CPU2 | grep Temp | awk '{print $8}')
+file=/home/nvidia/Testes_ARM/Monitor/temperature.dat
 
-#echo "$CURRENT_TEMP1  $CURRENT_TEMP2" >> tempetature.dat
-saida_file=/home/nvidia/Testes_ARM/Monitor/temperature.dat
-
-date +%H:%M:%S.%N >> $saida_file
-echo -n "BCPU:" >> $saida_file
-cat /sys/devices/virtual/thermal/thermal_zone0/temp >> $saida_file
-echo -n "MCPU:" >> $saida_file
-cat /sys/devices/virtual/thermal/thermal_zone1/temp >> $saida_file
-echo -n "GPU:" >> $saida_file
-cat /sys/devices/virtual/thermal/thermal_zone2/temp >> $saida_file
-echo -n "Board:" >> $saida_file
-cat /sys/devices/virtual/thermal/thermal_zone5/temp >> $saida_file 
+date +%H:%M:%S.%N >> $file
+echo -n "BCPU:" >> $file
+cat /sys/devices/virtual/thermal/thermal_zone0/temp >> $file
+echo -n "MCPU:" >> $file
+cat /sys/devices/virtual/thermal/thermal_zone1/temp >> $file
+echo -n "GPU:" >> $file
+cat /sys/devices/virtual/thermal/thermal_zone2/temp >> $file
+echo -n "Board:" >> $file
+cat /sys/devices/virtual/thermal/thermal_zone5/temp >> $file 
