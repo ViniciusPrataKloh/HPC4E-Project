bdd953ce0ce2e49eb74abd405e191b666e405ee9
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2017-10-25 13:20:37 -0200

    Auxiliar script temp_sgi.sh

--- temp_sgi.sh
+++ temp_sgi.sh
@@ -6,4 +6,4 @@ IPMI_TEMP_OUTPUT=`/usr/sbin/ipmi-sensors`
 CURRENT_TEMP1=$(echo $IPMI_TEMP_OUTPUT | /usr/sbin/ipmi-sensors | grep CPU1 | grep Temp | awk '{print $8}')
 CURRENT_TEMP2=$(echo $IPMI_TEMP_OUTPUT | /usr/sbin/ipmi-sensors | grep CPU2 | grep Temp | awk '{print $8}')
 
-echo "$CURRENT_TEMP1  $CURRENT_TEMP2" >> tempetature.dat
+echo "$CURRENT_TEMP1  $CURRENT_TEMP2" >> [OUTPUT_DIRECTORY]/tempetature.dat
